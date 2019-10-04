14b88896268f1a50ac13623ec16b9f8c1ca61660
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2019-10-04 14:36:36 -0300

    Deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://ebfccdf3.ngrok.io";
+const BACKEND_URL = "https://24a73e78.ngrok.io";
 
 export default function request(path, method, body, headers) {
     headers = new Headers(headers);
