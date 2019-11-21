10683d880df8cfb2890d8622126f9c6799717a6d
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2019-11-21 03:50:13 -0300

    Add info in about layout

--- src/componentsLayout/AboutLayout.js
+++ src/componentsLayout/AboutLayout.js
@@ -4,7 +4,14 @@ class AboutLayout extends Component {
   render() {
     return (
       <div>
-        <h1>Sobre</h1>
+        <div className="container">
+          <h1 className="font-weight-lighter">VisuED</h1>
+          <h2 className="font-weight-lighter">Implemente e visualize estruturas de dados</h2>
+          <p>
+            O VisuED é uma ferramenta online que permite a implementação e visualização de listas ligadas de forma dinâmica, para assim possibilitar ao usuário um maior entendimento prático sobre as manipulações de referências das listas ligadas. A ferramenta foi criada por <a href="https://github.com/davilaerte" target="_blank" rel="noopener noreferrer">Davi Laerte</a>, aluno do curso de Ciência da Computação na UFCG.
+          </p>
+          <h6 class="text-center"><a href="https://github.com/davilaerte/ed-visualization"  target="_blank" rel="noopener noreferrer">Link Para Repositório Do Site.</a></h6>
+        </div>
       </div>
     );
   }
